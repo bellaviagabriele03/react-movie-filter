3c4a409d74d8789752c1d19eafa6c3e6c0609996
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-12-02 16:50:37 +0100

    feat: installato bootstrap wip: inizio struttura pagina con suddivisione in componenti

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      
+      <div className="container">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam natus dolore suscipit nesciunt mollitia officia asperiores. Quo repudiandae quos, esse placeat pariatur facere explicabo excepturi nesciunt necessitatibus ipsum voluptatibus magni?</div>
     </>
   )
 }
