ce7ea06a9dafd1e507a92507546ce70627ac756e
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-12-02 17:58:58 +0100

    Feat: struttura form di base

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 
 import Card from "./Card";
+import Form from "./Form";
 
 function Main() {
 
@@ -9,7 +10,7 @@ function Main() {
                 <div className="row">
                     <Card />
                 </div>
-
+                <Form />
             </div>
         </main>
     )
